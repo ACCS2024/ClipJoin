ffeb3239d73939681a5160414894087ecef765c3
none <160735780+ACCS2024@users.noreply.github.com>
2026-04-10 14:43:59 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ClipJoin
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
